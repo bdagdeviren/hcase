dcdc2583f067b4bc90083ef9d3a5f8fd3a8de396
devops <devops@devops.com>
2023-02-08 16:23:54 +0300

    updated

--- certs.py
+++ certs.py
@@ -43,9 +43,50 @@ def get_expire_date():
     return fullString
 
 
+def get_expire_date_like_prometheus():
+    fullString = ""
+
+    files = run_fast_scandir(cert_dir, [".crt", ".key"])
+    for i in files:
+        if os.path.basename(i) in certificate_list:
+            f = open(i, "r")
+            x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, f.read())
+            bytesAfter = x509.get_notAfter()
+            timestampAfter = bytesAfter.decode('utf-8')
+            bytesBefore = x509.get_notBefore()
+            timestampBefore = bytesBefore.decode('utf-8')
+            dateTimeBefore = datetime.strptime(timestampBefore, '%Y%m%d%H%M%SZ').strftime(
+                "%b %d %H:%M:%S %Y")
+            dateTimeAfter = datetime.strptime(timestampAfter, '%Y%m%d%H%M%SZ').strftime(
+                "%b %d %H:%M:%S %Y")
+
+            print(dateTimeAfter)
+            print(dateTimeBefore)
+
+            d1 = datetime.strptime(dateTimeBefore, '%b %d %H:%M:%S %Y')
+            d2 = datetime.strptime(dateTimeAfter, '%b %d %H:%M:%S %Y')
+
+            check = 0
+            if (d2-d1).days < 30:
+                check = 1
+            else:
+                check = 0
+
+            fullString += "certificate_expire_check{cert_name=" + os.path.basename(
+                i) + ",not_after=" + dateTimeBefore + ",not_before=" + dateTimeAfter+"} " + str(check)
+
+    return fullString
+
+
 @app.get("/", response_class=PlainTextResponse)
 async def read_root():
     fullString = get_expire_date()
+    return fullString
+
+
+@app.get("/metrics", response_class=PlainTextResponse)
+async def read_root():
+    fullString = get_expire_date_like_prometheus()
     return fullString
 
 
